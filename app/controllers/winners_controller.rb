d2fa3591e1d33cb6788c1fb6ac97b94b5ea3ccac
swistak35 <swistak35@raptop.(none)>
2010-07-15 10:33:28 +0200

    update winners controller

--- app/controllers/winners_controller.rb
+++ app/controllers/winners_controller.rb
@@ -38,7 +38,7 @@ class WinnersController < ApplicationController
   end
   
   def choose_reward
-    available_rewards = Reward.all
+    available_rewards = Reward.where(["? > ?", @winner.fraction, 0])
     array = []
     available_rewards.each do |reward|
       howmany = reward.rate * reward.ally if @winner.fraction == 'ally'
